bfb10a59c1fd875ca01221bf61f70528845fad7f
fichel <861559021@qq.com>
2019-05-17 12:39:49 +0800

    初次提交

---
+++ src/main/java/com/fichel/dpattern/App.java
@@ -0,0 +1,13 @@
+package com.fichel.dpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
